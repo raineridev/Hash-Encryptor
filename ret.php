d0a8d2b97ef9c75e2321c0aab4b60905343deb77
raineridev <116137268+raineridev@users.noreply.github.com>
2023-06-07 21:08:54 -0300

    Add files via upload

---
+++ ret.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hash Generator</title>
+    <style>
+        * {
+            text-align: center;
+            font-family: 'Poppins', sans-serif;
+            src: url('Poppins-Medium.ttf') format('truetype');
+        }
+        #send, #value {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hash Generator</h1>
+    <form method="POST" action="">
+        <label>String you want to change to BCRYPT</label><br>
+        <input type="text" name="value" id="value"><br>
+        <input type="submit" id="send" value="Send">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $password = $_POST['value'];
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        echo "Your hash:<br>" . $hash;
+    }
+    ?>
+</body>
+</html>
